e58840cfaeb64d1e038b237ef404d0a36b6abb20
rushabhp99@gmail.com <rushabhp99@gmail.com>
2016-07-28 20:25:35 +0530

    Now you can extract apk

--- app/src/main/java/rushabh/apk/extracter/AppInfoAdapter.java
+++ app/src/main/java/rushabh/apk/extracter/AppInfoAdapter.java
@@ -1,22 +1,43 @@
 package rushabh.apk.extracter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AppInfoAdapter extends RecyclerView.Adapter<AppInfoAdapter.MyViewHolder> {
 
     private ArrayList<AppInfo> dataSet;
+    public static String PATH = Environment.getExternalStorageDirectory() + "/APK-Extract";
     private Context mContext;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -24,14 +45,14 @@ public class AppInfoAdapter extends RecyclerView.Adapter<AppInfoAdapter.MyViewHo
         TextView name;
         TextView package_name;
         ImageView image;
-        ImageButton option;
+        CardView option;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             this.name = (TextView) itemView.findViewById(R.id.name);
             this.package_name = (TextView) itemView.findViewById(R.id.package_name);
             this.image = (ImageView) itemView.findViewById(R.id.image);
-            this.option = (ImageButton) itemView.findViewById(R.id.option);
+            this.option = (CardView) itemView.findViewById(R.id.card_view);
         }
     }
 
@@ -53,11 +74,18 @@ public class AppInfoAdapter extends RecyclerView.Adapter<AppInfoAdapter.MyViewHo
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
 
-        AppInfo app = dataSet.get(listPosition);
+       final  AppInfo app = dataSet.get(listPosition);
 
         holder.name.setText(app.getApp_name());
         holder.package_name.setText(app.getPackage_name());
         holder.image.setBackground(app.getApp_image());
+
+        holder.option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getApk(app.getPackage_name());
+            }
+        });
 
     }
 
@@ -65,4 +93,51 @@ public class AppInfoAdapter extends RecyclerView.Adapter<AppInfoAdapter.MyViewHo
     public int getItemCount() {
         return dataSet.size();
     }
+
+
+    public void init_direcctory(){
+        File folder = new File(PATH);
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+    }
+
+    public void getApk(String packagename){
+        final PackageManager pm = mContext.getPackageManager();
+//get a list of installed apps.
+        List<ApplicationInfo> packages =  pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            if(packageInfo.packageName.matches(packagename)) {
+
+                    Log.d(packageInfo.publicSourceDir, packageInfo.packageName + "    " + PATH + packageInfo.packageName);
+                try {
+                    copyFile(packageInfo.publicSourceDir, PATH + "/" + packageInfo.loadLabel(mContext.getPackageManager()).toString() + ".apk");
+                } catch (Exception e){
+
+                }
+                Toast.makeText(mContext,"APK extracted successfully in\n" +  PATH+"/" + packageInfo.loadLabel(mContext.getPackageManager()).toString()+".apk",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+    }
+
+  public  void copyFile(String s, String d) throws IOException {
+      File src = new File(s);
+      File dst = new File(d);
+        FileChannel inChannel = new FileInputStream(src).getChannel();
+        FileChannel outChannel = new FileOutputStream(dst).getChannel();
+        try {
+            inChannel.transferTo(0, inChannel.size(), outChannel);
+        } finally {
+            if (inChannel != null)
+                inChannel.close();
+            if (outChannel != null)
+                outChannel.close();
+        }
+    }
+
+
 }
--- app/src/main/java/rushabh/apk/extracter/MainActivity.java
+++ app/src/main/java/rushabh/apk/extracter/MainActivity.java
@@ -1,8 +1,13 @@
 package rushabh.apk.extracter;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,25 +18,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static RecyclerView.Adapter adapter;
+    public static Activity ac;
     private RecyclerView.LayoutManager layoutManager;
     private static RecyclerView recyclerView;
     private static ArrayList<AppInfo> data;
+    private static ArrayList<AppInfo> datai;
     private Switch toggle;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ac = this;
+
         data = new ArrayList<AppInfo>();
+        datai = new ArrayList<AppInfo>();
 
         recyclerView = (RecyclerView) findViewById(R.id.app_list);
         toggle = (Switch) findViewById(R.id.toggle);
@@ -40,25 +62,148 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (toggle.isChecked() == true) {
+                    new loadlist(true).execute();
+                } else {
+                    new loadlist(false).execute();
+                }
+            }
+        });
 
-        adapter = new AppInfoAdapter(MainActivity.this, data);
-        recyclerView.setAdapter(adapter);
+        if (toggle.isChecked() == true) {
+            new loadlist(true).execute();
+        } else {
+            new loadlist(false).execute();
+        }
 
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void getapplist_installed(){
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://rushabh.apk.extracter/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://rushabh.apk.extracter/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    private class loadlist extends AsyncTask<Void, Void, Void> {
+
+        boolean sys;
+        ProgressDialog progressBar;
+
+        public loadlist(boolean system) {
+            this.sys = system;
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+            progressBar = new ProgressDialog(MainActivity.this);
+            progressBar.setCancelable(true);
+            progressBar.setMessage("Getting info...");
+            progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressBar.setProgress(0);
+            progressBar.setMax(100);
+            progressBar.show();
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            if (sys == true) {
+                getapplist_system();
+                Collections.sort(data, new Comparator<AppInfo>() {
+                    public int compare(AppInfo v1, AppInfo v2) {
+                        return v1.getApp_name().compareTo(v2.getApp_name());
+                    }
+                });
+            } else {
+                getapplist_onstalled();
+                Collections.sort(datai, new Comparator<AppInfo>() {
+                    public int compare(AppInfo v1, AppInfo v2) {
+                        return v1.getApp_name().compareTo(v2.getApp_name());
+                    }
+                });
+            }
+
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(sys == true) {
+                adapter = null;
+                adapter = new AppInfoAdapter(MainActivity.this, data);
+                recyclerView.setAdapter(adapter);
+            }else {
+                adapter = null;
+                adapter = new AppInfoAdapter(MainActivity.this, datai);
+                recyclerView.setAdapter(adapter);
+            }
+            progressBar.dismiss();
+            super.onPostExecute(aVoid);
+
+        }
+    }
+
+
+    public void getapplist_onstalled() {
 
         try {
-            if (data != null) {
-                data.clear();
+            if (datai != null) {
+                datai.clear();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
         List<PackageInfo> apps = getPackageManager().getInstalledPackages(0);
 
-        for(int i=0;i<apps.size();i++) {
+        for (int i = 0; i < apps.size(); i++) {
             PackageInfo p = apps.get(i);
 
             if ((p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
@@ -66,24 +211,26 @@ public class MainActivity extends AppCompatActivity {
                 newInfo.setApp_name(p.applicationInfo.loadLabel(getPackageManager()).toString());
                 newInfo.setPackage_name(p.packageName);
                 newInfo.setApp_image(p.applicationInfo.loadIcon(getPackageManager()));
-                data.add(newInfo);
+                datai.add(newInfo);
             }
         }
     }
 
-    public void getapplist_system(){
+
+
+    public void getapplist_system() {
 
         try {
             if (data != null) {
                 data.clear();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
         List<PackageInfo> apps = getPackageManager().getInstalledPackages(0);
 
-        for(int i=0;i<apps.size();i++) {
+        for (int i = 0; i < apps.size(); i++) {
             PackageInfo p = apps.get(i);
 
             if ((p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
